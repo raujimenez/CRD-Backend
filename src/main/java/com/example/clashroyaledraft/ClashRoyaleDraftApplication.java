3f807bca078c6bb200e532b6a5b1843a0ed391d0
Raul Jimenez <officialrauljimenez@gmail.com>
2021-03-26 15:12:16 -0500

    first commit

---
+++ src/main/java/com/example/clashroyaledraft/ClashRoyaleDraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.clashroyaledraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashRoyaleDraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashRoyaleDraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clashroyaledraft/ClashRoyaleDraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clashroyaledraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClashRoyaleDraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
